151baa1523f62a4578eaa129b1d87eab7a570c8c
Thais Guedes <thaisferguedes@hotmail.com>
2025-05-15 18:29:08 -0300

    Create raiz_quadrada.cpp

---
+++ exercises/raiz_quadrada.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n1 = 4;
+    double n2 = 12.25, n3 = 0.0121;
+
+    cout << "Raiz quadrada de " << n1 << " = " << sqrt(n1) << endl;
+    cout << "Raiz quadrada de " << n2 << " = " << sqrt(n2) << endl;
+    cout << "Raiz quadrada de " << n3 << " = " << sqrt(n3) << endl;
+
+    return 0;
+}
